1b4a5d815465fe9897ed192537cbb2d9cbb21f0c
s3verus <en.severus@gmail.com>
2022-02-07 20:09:04 +0330

    remove useless functions, try to working with Result<>, ...

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,46 @@
 use redis::Commands;
 use std::io::prelude::*;
 use std::net::TcpStream;
+use redis::RedisError;
 
 pub fn connect() -> redis::Connection {
     let redis_host_name = "127.0.0.1:6379";
     let redis_password = "";
     let redis_conn_url = format!("{}://:{}@{}", "redis", redis_password, redis_host_name);
 
-    redis::Client::open(redis_conn_url)
+    redis::Client::open(redis_conn_url)//?
         .expect("Invalid connection URL")
-        .get_connection()
+        .get_connection()//?
         .expect("failed to connect to Redis")
 }
 
-pub fn block_domain(domain: &str) {
+pub fn block_domain(domain: &str) -> Result<(), RedisError> {
     let mut conn = connect();
     let _: () = redis::cmd("rpush")
         .arg("block_list")
         .arg(domain)
-        .query(&mut conn)
-        .expect("failed to rpush domain!");
-
-    //println!("domain {}, blocked!", domain);
+        .query(&mut conn)?;
+    Ok(())
 }
 
-pub fn release_domain(domain: &str) {
-    println!("domain {}, released!", domain);
-}
-
-pub fn read_block_list() -> Vec<String> {
+pub fn is_exists(domain: &String) -> Result<bool, RedisError> {
     let mut conn = connect();
     let block_list: Vec<String> = conn
-        .lrange("block_list", 0, -1)
-        .expect("failed to execute LRANGE for 'items'");
-
-    block_list
-}
-
-pub fn is_exists(domain: &String) -> bool {
-    let block_list = read_block_list();
+        .lrange("block_list", 0, -1)?;
+    
     if block_list.contains(domain) {
-        true
+        Ok(true)
     } else {
-        false
+        Ok(false)
     }
 }
 
-pub fn handle_connection(mut stream: TcpStream) {
+pub fn handle_connection(mut stream: TcpStream) -> Result<(), RedisError> {
     let mut buffer = [0; 256];
     let post_block = b"POST /block HTTP/1.1\r\n";
     let post_check = b"POST /check HTTP/1.1\r\n";
 
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer)?; // TODO handle it, has different error type!
     println!("Request:\n{}", String::from_utf8_lossy(&buffer[..]));
 
     // parse data
@@ -64,10 +53,17 @@ pub fn handle_connection(mut stream: TcpStream) {
 
     // Validating the Request and Selectively Responding
     let response = if buffer.starts_with(post_block) {
-        block_domain(&site);
+        match block_domain(&site) {
+            Ok(()) => (),
+            Err(e) => return Err(e),
+        }
         "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nsite blocked!"
     } else if buffer.starts_with(post_check) {
-        if is_exists(&site) {
+        let result = match is_exists(&site) {
+            Ok(boolean) => boolean,
+            Err(e) => return Err(e),
+        };
+        if result {
             "HTTP/1.1 200 OK\r\nContent-Length: 19\r\n\r\nit's in block list!"
         } else {
             "HTTP/1.1 200 OK\r\nContent-Length: 24\r\n\r\nnot found in block list!"
@@ -76,6 +72,7 @@ pub fn handle_connection(mut stream: TcpStream) {
         "HTTP/1.1 404 NOT FOUND\r\nContent-Length: 14\r\n\r\nwrong request!"
     };
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write(response.as_bytes())?; // TODO handle
+    stream.flush()?; // TODO handle
+    Ok(())
 }
