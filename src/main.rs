c033a7560fc13ca36bd23bd46ee4888c3627ca3d
s3verus <en.severus@gmail.com>
2022-02-08 22:59:31 +0330

    read tcplistener bind 'host:port' from config file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
+    
+    let docs = work_with_redis::yaml_object().unwrap();
+
+    // Multi document support, doc is a yaml::Yaml
+    let doc = &docs[0];
+
+    // read bind from Config.yaml
+    let bind = doc["config"]["bind"].as_str().unwrap();
+    let listener = TcpListener::bind(bind).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
