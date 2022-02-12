e51bceef4d30cde2f78747bf57938313f1a1e8c4
s3verus <en.severus@gmail.com>
2022-02-12 19:51:43 +0330

    add_items and get_items added to dao

--- src/dao.rs
+++ src/dao.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 use crate::config::DB;
+use redis::RedisError;
+use redis::Commands;
 
 pub fn connect(config: DB) -> Result<redis::Connection, Box<dyn Error>> {
     let host = config.host;
@@ -9,4 +11,16 @@ pub fn connect(config: DB) -> Result<redis::Connection, Box<dyn Error>> {
 
     let result = redis::Client::open(conn_url)?.get_connection()?;
     Ok(result)
+}
+
+pub fn add_items(key: &str, value: &str, mut conn: redis::Connection) -> Result<(), RedisError> {
+    let _: () = redis::cmd("rpush")
+        .arg(key)
+        .arg(value)
+        .query(&mut conn)?;
+    Ok(())
+}
+
+pub fn get_items(list_name: &str, mut conn: redis::Connection) -> Result<Vec<String>, RedisError> {
+    conn.lrange(list_name, 0, -1)
 }
