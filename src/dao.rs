193c671d29d97fce94f88f8db2c2c1e654376dec
s3verus <en.severus@gmail.com>
2022-02-12 18:54:27 +0330

    change signature, arg Config to DB

--- src/dao.rs
+++ src/dao.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
+use crate::config::DB;
 
-pub fn connect(config: Config) -> Result<redis::Connection, Box<dyn Error>> {
-    let host = config.db.host;
-    let port = config.db.port;
-    let pass = config.db.pass;
+pub fn connect(config: DB) -> Result<redis::Connection, Box<dyn Error>> {
+    let host = config.host;
+    let port = config.port;
+    let pass = config.pass;
     let conn_url = format!("{}://:{}@{}:{}", "redis", pass, host, port);
 
     let result = redis::Client::open(conn_url)?.get_connection()?;
