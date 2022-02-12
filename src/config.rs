4ed19c02fa7f378d1b43226118e003b36787459d
s3verus <en.severus@gmail.com>
2022-02-12 18:51:50 +0330

    mv structs to config.rs, rm structs.rs, use serde in load_config()

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,40 @@
 use std::fs::File;
 use std::io::Read;
-extern crate yaml_rust;
 use std::error::Error;
-use yaml_rust::Yaml;
-use yaml_rust::YamlLoader;
+use serde::Deserialize;
 
-pub fn load_config() -> Result<Yaml, Box<dyn Error>> {
+#[derive(Clone)]
+#[derive(Deserialize, Debug)]
+pub struct Listener {
+    pub bind: String,
+}
+
+#[derive(Clone)]
+#[derive(Deserialize, Debug)]
+pub struct DB {
+    pub host: String,
+    pub port: String,
+    pub pass: String,
+    pub user: String,
+}
+
+#[derive(Clone)]
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub listener: Listener,
+    pub db: DB,
+}
+
+pub fn load_config() -> Result<Config, Box<dyn Error>> {
     // Open file
-    let mut file = File::open("Config.yaml")?;
+    let mut file = File::open("Config.json")?;
 
     // Read the file contents into a string
     let mut s = String::new();
     file.read_to_string(&mut s)?;
 
-    let docs = YamlLoader::load_from_str(&s)?;
+    let config: Config = serde_json::from_str(&s)?;
 
-    // Multi document support, doc is a yaml::Yaml
-    Ok(yaml_rust::Yaml::Array(docs))
+    // println!("config: {:?}", config);
+    Ok(config)
 }
