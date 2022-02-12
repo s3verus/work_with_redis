03f243b26199759050f7087e71dec27b04aacc13
s3verus <en.severus@gmail.com>
2022-02-12 18:56:44 +0330

    clean codes,

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
 use std::net::TcpListener;
-use yaml_rust::Yaml;
-use std::error::Error;
+use work_with_redis::config::load_config;
+use work_with_redis::manager::handle_connection;
 
 fn main() {
-    let config = config::load_config().unwrap();
+    let config = load_config().unwrap();
 
-    // Multi document support, doc is a yaml::Yaml
-    let conf = &config[0];
-    
-    let config = match structs::set_config(conf) {
-        Ok(ok) => ok,
-        Err(_) => return (), // TODO handle it
-    };
-
-    // read bind from Config.yaml
-    let bind = config.bind.bind;
-    let listener = TcpListener::bind(bind).unwrap();
+    let listener = TcpListener::bind(config.listener.bind).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let results = manager::handle_connection(stream, config);
+        let results = handle_connection(stream, config.db.clone());
         println!("{:?}", results);
     }
 }
