921bbde758462807e6788d623822cac8adb0e095
s3verus <en.severus@gmail.com>
2022-02-14 09:11:30 +0330

    set mut static

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
 use std::net::TcpListener;
 use work_with_redis::config::load_config;
 use work_with_redis::manager::handle_connection;
-use work_with_redis::dao::{connect, get_items};
-use work_with_redis::manager::List;
+use work_with_redis::manager::{MY_STRUCT, MyStruct};
 
 fn main() {
     let config = load_config().unwrap();
 
     let listener = TcpListener::bind(config.listener.bind).unwrap();
-
-    let conn = connect(config.db.clone()).unwrap();
-    let list = List { 
-        block: get_items("block_list", conn).unwrap(),
-    };
+    
+    // Setting a MutStatic
+    MY_STRUCT.set(MyStruct::update()).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let results = handle_connection(stream, config.db.clone(), &list);
+        let results = handle_connection(stream, config.db.clone());
         println!("{:?}", results);
     }
 }
