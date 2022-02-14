aa9c213b7dee98e870b5c2702af5196e8e274f56
s3verus <en.severus@gmail.com>
2022-02-14 09:13:52 +0330

    implement mut static logic for get data from database and save in global variable

--- src/manager.rs
+++ src/manager.rs
@@ -4,22 +4,42 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use crate::dao::*;
 use crate::config::DB;
+use crate::config::load_config;
+use lazy_static::lazy_static;
+use mut_static::MutStatic;
+use std::ops::DerefMut;
+use std::mem;
 
+#[derive(Debug)]
 #[derive(Clone)]
-pub struct List {
-    pub block: Vec<String>,
-    // pub white: Vec<String>,
+pub struct MyStruct {
+    pub list: Vec<String>,
 }
 
-pub fn block_domain(domain: &str, mut conn: redis::Connection) -> Result<List, RedisError> {
-    let _: () = add_items("block_list", domain, &mut conn)?;
-    let list = List {
-        block: get_items("block_list", conn)?,
-        // white_list: get_items("white_list")?,
+impl MyStruct {
+    pub fn update() -> Self {
+        let config = load_config().unwrap();
+        let conn = connect(config.db.clone()).unwrap();
+        let list = get_items("block_list", conn).unwrap();
+        Self { list }
+    }
+}
+
+// Declaring a MutStatic
+lazy_static! {
+    pub static ref MY_STRUCT: MutStatic<MyStruct> = {
+        MutStatic::new()
     };
-    Ok(list)
 }
 
+pub fn block_domain(domain: &str, mut conn: redis::Connection) -> Result<(), RedisError> {
+    let _: () = add_items("block_list", domain, &mut conn)?;
+    // Resetting a MutStatic
+    let mut my_struct = MY_STRUCT.write().unwrap();
+    mem::replace(my_struct.deref_mut(), MyStruct::update());
+    Ok(())
+}
+/*
 pub fn is_exists(domain: &String, conn: redis::Connection) -> Result<bool, RedisError> {
     let block_list: Vec<String> = get_items("block_list", conn)?;
 
@@ -29,14 +49,18 @@ pub fn is_exists(domain: &String, conn: redis::Connection) -> Result<bool, Redis
         Ok(false)
     }
 }
-pub fn is_exists2(domain: &String, list: &List) -> bool {
-    if list.block.contains(domain) {
+*/
+pub fn is_exists(domain: &String) -> bool {
+    // Using a MutStatic
+    let result = MY_STRUCT.read().unwrap();
+    let result = result.list.contains(domain);
+    if result {
         true
     } else {
         false
     }
 }
-pub fn handle_connection(mut stream: TcpStream, config: DB, list: &List ) -> Result<(), Box<dyn Error>> {
+pub fn handle_connection(mut stream: TcpStream, config: DB) -> Result<(), Box<dyn Error>> {
     let mut buffer = [0; 256];
     let post_block = b"POST /block HTTP/1.1\r\n";
     let post_check = b"POST /check HTTP/1.1\r\n";
@@ -55,12 +79,11 @@ pub fn handle_connection(mut stream: TcpStream, config: DB, list: &List ) -> Res
 
     // Validating the Request and Selectively Responding
     let response = if buffer.starts_with(post_block) {
-        // TODO how make it global?
-        let list: List = block_domain(&site, conn)?;
+        block_domain(&site, conn)?;
         "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nsite blocked!"
     } else if buffer.starts_with(post_check) {
         // let result = is_exists(&site, conn)?;
-        let result = is_exists2(&site, list);
+        let result = is_exists(&site);
         if result {
             "HTTP/1.1 200 OK\r\nContent-Length: 19\r\n\r\nit's in block list!"
         } else {
